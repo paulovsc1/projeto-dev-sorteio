f00cd92098bc2261637aa639d611179737e2520d
Paulo Victor da Silva Carlos <paulopbv2418@gmail.com>
2025-05-28 21:17:25 -0300

    atualizei meu código

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
 function generateNumber() {
-    alert('vc clicou no botao')
+
+    const  min = Math.ceil (document.querySelector(".input-min").value)
+    const  max = Math.floor (document.querySelector(".input-max").value)
+    
+     if (min >= max) {
+        alert("erro")
+    } else {
+    const result = Math.floor(Math.random() * (max - min + 1) + min);
+    alert (result)
+
+}
+   
 }
